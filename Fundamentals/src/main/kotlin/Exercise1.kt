cee67b3eb9bf24bcce99a064607e810da8c42c7c
Alexander Ferreira <alexander.ferreira1@hotmail.com>
2023-10-13 16:16:31 -0300

    feat(Fundamentals): add new exercise about variables.

---
+++ Fundamentals/src/main/kotlin/Exercise1.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var mutableValue: Int = 6
+    mutableValue = 5
+    println(mutableValue)
+
+    val immutableValue: Int = 6
+    // immutableValue = 7           Val cannot be reassigned
+}
