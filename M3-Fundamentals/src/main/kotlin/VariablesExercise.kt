7e982654b4b0b8330a8adf4534d3967df464009c
Alexander Ferreira <48698501+aleferreira01@users.noreply.github.com>
2023-10-26 20:32:47 -0300

    Delete M3-Fundamentals directory

--- M3-Fundamentals/src/main/kotlin/VariablesExercise.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    var mutableValue: Int = 6
-    mutableValue = 5
-    println(mutableValue)
-
-    val immutableValue: Int = 6
-    // immutableValue = 7           Val cannot be reassigned
-}
